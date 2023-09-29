5c8b5668f2feda8f0297992514e5c406ca5c443b
Sagar <sagar.dash290@gmail.com>
2023-09-29 16:25:15 +0600

    feat: add product api

--- plugin-api.php
+++ plugin-api.php
@@ -6,19 +6,28 @@ add_action('wp_ajax_get_cities', 'pt_hms_ajax_get_cities');
 add_action('wp_ajax_get_zones', 'pt_hms_ajax_get_zones');
 add_action('wp_ajax_get_areas', 'pt_hms_ajax_get_areas');
 add_action('wp_ajax_create_order', 'ajax_pt_hms_create_new_order');
+add_action('rest_api_init', function () {
+    register_rest_route(PTC_PLUGIN_PREFIX . '/v1', '/products/(?P<id>\d+)', array(
+        'methods' => 'GET',
+        'callback' => 'product_details_callback',
+    ));
+});
 
-function pt_hms_ajax_get_stores() {
+function pt_hms_ajax_get_stores()
+{
     $stores = pt_hms_get_stores();
     wp_send_json_success($stores);
 }
 
-function pt_hms_ajax_get_cities() {
+function pt_hms_ajax_get_cities()
+{
     $cities = pt_hms_get_cities();
     wp_send_json_success($cities);
 }
 
-function pt_hms_ajax_get_zones() {
-    if(isset($_POST['city_id'])) {
+function pt_hms_ajax_get_zones()
+{
+    if (isset($_POST['city_id'])) {
         $city_id = intval($_POST['city_id']);
         $zones = pt_hms_get_zones($city_id);
         wp_send_json_success($zones);
@@ -27,8 +36,9 @@ function pt_hms_ajax_get_zones() {
     }
 }
 
-function pt_hms_ajax_get_areas() {
-    if(isset($_POST['zone_id'])) {
+function pt_hms_ajax_get_areas()
+{
+    if (isset($_POST['zone_id'])) {
         $zone_id = intval($_POST['zone_id']);
         $areas = pt_hms_get_areas($zone_id);
         wp_send_json_success($areas);
@@ -37,7 +47,8 @@ function pt_hms_ajax_get_areas() {
     }
 }
 
-function ajax_pt_hms_create_new_order() {
+function ajax_pt_hms_create_new_order()
+{
     // Check nonce for security
     // check_ajax_referer('create_new_order_nonce', 'nonce');
 
@@ -53,3 +64,14 @@ function ajax_pt_hms_create_new_order() {
     // Always die in functions echoing AJAX content
     die();
 }
+
+function product_details_callback(WP_REST_Request $request)
+{
+    $orderId =  $request->get_param('id');
+
+    $orderData = wc_get_order($orderId);
+
+    wp_send_json_success($orderData->get_data());
+}
+
+
